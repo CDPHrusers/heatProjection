ae68d33bd46ea3937f424f6006a0a94e330fe923
Marisa Donnelly <madonnelly@ucdavis.edu>
2019-04-12 11:03:15 -0700

    Added to filterED function
    Added bobbCodes and numDiagnosis arguments to filteredED function to allow for defining these two values in the function. There might be a better way to do this though.

--- ED_temp_combine_MD.R
+++ ED_temp_combine_MD.R
@@ -18,10 +18,92 @@ library(parallel)
 bobbCodes <- c(55L, 157L, 159L, 244L, 108L, 2L)
 
 ##create function to read in and filter the hospital data based on Bobb codes of interest
-filterED <- function(year) {
+filterED <- function(bobbCodes, year, numDiagnosis) {
   file<- paste0("ED/cdph_ed_rln",year,".csv")
   foo <- fread(paste(file))
   
+  if (numDiagnosis=1){
+     setkey(foo, ccs_dx_prin)
+ ##pull out all cases that have the designated Bobb codes in either the primary, secondary or tertiary diagnosis columns
+ ##bring in patient's zipcode, county code, and date of service
+  out <-
+    foo [ccs_dx_prin %in% bobbCodes , c(
+             "dx_prin",
+             "ec_prin",
+             "ccs_dx_prin",
+             "fac_id",
+             "pat_type",
+             "lic_type",
+             "agdyserv",
+             "agyrserv",
+             "sex",
+             "patzip",
+             "patco",
+           # "serv_q",
+           # "serv_d",
+           # "serv_m",
+           # "serv_y",
+             "dispn",
+             "payer",
+             "pr_prin",
+             "opr1",
+             "serv_dt",
+             "brthdate",
+           # "dob_raw",
+             "faczip",
+             "fac_co",
+             "rln",
+             "race_grp"
+           )] %>%
+  ##count the number of cases by patient zip code and by date
+  .[,n :=.n, by=.(patzip, serv_dt)]%>%
+  .[, Date:= as.Date(serv_dt, format = "%m/%d/%Y")]
+  
+  return(out)
+    
+    } else if (numDiagnosis=2) {
+    ##set key to primary, secondary, and tertiary ccs diagnosis columns
+  setkey(foo, ccs_dx_prin, ccs_odx1)
+ ##pull out all cases that have the designated Bobb codes in either the primary, secondary or tertiary diagnosis columns
+ ##bring in patient's zipcode, county code, and date of service
+  out <-
+    foo [ccs_dx_prin %in% bobbCodes |
+           ccs_odx1 %in% bobbCodes, c(
+             "dx_prin",
+             "ec_prin",
+             "ccs_dx_prin",
+             "ccs_odx1",
+             "fac_id",
+             "pat_type",
+             "lic_type",
+             "agdyserv",
+             "agyrserv",
+             "sex",
+             "patzip",
+             "patco",
+           # "serv_q",
+           # "serv_d",
+           # "serv_m",
+           # "serv_y",
+             "dispn",
+             "payer",
+             "pr_prin",
+             "opr1",
+             "serv_dt",
+             "brthdate",
+           # "dob_raw",
+             "faczip",
+             "fac_co",
+             "rln",
+             "race_grp"
+           )] %>%
+  ##count the number of cases by patient zip code and by date
+  .[,n :=.n, by=.(patzip, serv_dt)]%>%
+  .[, Date:= as.Date(serv_dt, format = "%m/%d/%Y")]
+  
+  return(out)
+    
+    } else if (numDiagnosis=3) {
  ##set key to primary, secondary, and tertiary ccs diagnosis columns
   setkey(foo, ccs_dx_prin, ccs_odx1, ccs_odx2)
  ##pull out all cases that have the designated Bobb codes in either the primary, secondary or tertiary diagnosis columns
@@ -64,6 +146,10 @@ filterED <- function(year) {
   .[, Date:= as.Date(serv_dt, format = "%m/%d/%Y")]
   
   return(out)
+  
+  }
+  else 
+    print("The number of diagnoses did not match the function criteria")
   
 }
 
