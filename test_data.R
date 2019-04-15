87dfc9ff8e1759ca4520d188c46824d3dd5dd615
Marisa Donnelly <madonnelly@ucdavis.edu>
2019-04-15 07:19:04 -0700

    Create test test_data.R
    Created a code to subset the data for testing functions

---
+++ test_data.R
@@ -0,0 +1,11 @@
+##code to create a subset of data for testing functions
+
+data<- fread("/data/tempAndED_allYears.csv")
+
+# sample 5% of data
+test_index <- sample(1:nrow(data), 0.05 * nrow(data), replace=FALSE)
+
+# subset data
+sub_data <- data[test_index,]
+
+fwrite(sub_data, "/data/sub_tempAndED_allYears.csv")
