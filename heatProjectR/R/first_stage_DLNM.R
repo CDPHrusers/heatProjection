498e1d84b4e52fa25742c88404f15dc0eb76a54b
Marisa Donnelly <madonnelly@ucdavis.edu>
2019-05-04 10:49:07 -0700

    Create function for first stage of DLNM analysis
    There are a few things I want to take out but am not ready to delete (ex. observations that did not converge that I removed from the datatable...)

---
+++ heatProjectR/R/first_stage_DLNM.R
@@ -0,0 +1,129 @@
+
+#' first_stage_DLNM
+#'
+#' This function conducts the first stage of the DLNM analysis described in DOI:https://doi.org/10.1016/S0140-6736(14)62114-0
+#' @param file_path The file path to the ED data you wish to process. Defaults to test data on the R server at CDPH
+#' @param varfun  Defines the type of spline used in the exposure function. Defaults to a B-spline.
+#' @param vardegree  Define the number of degrees of freedom in the type of spline defined by varfun.
+#' @param varper Defines that percentile at which to put the knots in the exposure function.
+#' @param lag Defines the lag in days.
+#' @param lagnk Defines the number of knots in the lag.
+#' @param dfseas Defines the degrees of freedom for seasons i.e. number of knots per year. 
+#' @param year Defines the number of years you are modeling.
+
+library(data.table)
+library(tidyverse)
+library(parallel)
+library(dlnm) 
+library(mvmeta)
+library(splines) 
+library(tsModel)
+
+
+first_stage_DLNM <- function (file_path="R:/heatProjections/data/processed/ed_test_data.csv", varfun = "bs",
+                              vardegree = 2, varper = c(10,75,90), lag = 3, lagnk = 3, 
+                              dfseas = 8, year = 13){
+  
+  # CHECK VERSION OF THE PACKAGE
+  if(packageVersion("dlnm")<"2.2.0")
+    stop("update dlnm package to version >= 2.2.0")
+  
+  # LOAD THE DATASET (CA zip temps and ED counts by day)
+  allZips <- fread(paste(file_path))
+  setkey(allZips, ZCTA)
+  # restrict data to only zipcodes with cases -> I am second guessing this
+  #zipsWithCases <- allZips[, .(total = sum(n, na.rm=T)), by= "ZCTA"] %>% .[total>0, "ZCTA"]
+  
+  ##define data for model to run on
+  #zipCA <- allZips[.(zipsWithCases)] 
+  zipCA <- allZips
+  zipCA$Date <- as.Date(zipCA$Date)
+  
+  # ARRANGE THE DATA AS A LIST OF DATA SETS
+  zips <- as.character(unique(zipCA$ZCTA)) #used to be regions
+  dlist <- lapply(zips,function(x) zipCA[zipCA$ZCTA==x,])
+  names(dlist) <- zips
+  
+  # METADATA FOR LOCATIONS
+  zips_meta <- data.frame(
+    zip = zips,
+    zipname = zips
+  )
+  
+ 
+  ord <- order(zips_meta$zipname)
+  dlist <- dlist[ord]
+  zips_meta <- zips_meta[ord,]
+  # these are indeces for the zipcodes that do not converge, as determined by running the model previously
+  #non.converge<-c(10, 13, 22, 40, 89, 111, 121, 123, 129, 139, 145, 180, 181, 183, 189, 207, 219, 228, 243, 265, 310)
+  #dlist.non<-dlist[non.converge]
+  #dlist.non.names<-names(dlist.non)
+ 
+  # count the number of cases in each zipcode and create new datatable
+  sum.n<-zipCA %>% group_by(ZCTA) %>% summarise(sum_n= sum(n))
+  # create column that defines if the zipcode converged or not
+  #sum.n$converge<- ifelse(sum.n$ZCTA %in% dlist.non.names, "n","y")
+  
+  # remove the zipcodes that did not converge
+  #dlist<-dlist[-c(10, 13, 22, 40, 89, 111, 121, 123, 129, 139, 145, 180, 181, 183, 189, 207, 219, 228, 243, 265, 310)]
+  
+  # remove the zipcodes that did not converge
+  #zip_met<-zip_met[-c(10, 13, 22, 40, 89, 111, 121, 123, 129, 139, 145, 180, 181, 183, 189, 207, 219, 228, 243, 265, 310),]
+  
+  # REMOVE ORIGINALS
+  rm(zipCA,zips,ord) 
+  
+  # COMPUTE PERCENTILES
+  per <- t(sapply(dlist,function(x) 
+    quantile(x$tmean_mean,c(2.5,10,25,50,75,90,97.5)/100,na.rm=T)))
+  
+  # MODEL FORMULA dow=day of week, cb= crossbasis ns=function for natural spline?
+  formula <- n~cb+ns(Date,df=dfseas*length(unique(year)))
+  
+  
+  # COEFFICIENTS AND VCOV FOR OVERALL CUMULATIVE SUMMARY
+  coef <- matrix(NA,nrow(zips_meta),length(varper)+vardegree,
+                 dimnames=list(zips_meta$zip))
+  vcov <- vector("list",nrow(zips_meta))
+  names(vcov) <- zips_meta$zip
+  
+  ################################################################################
+  # RUN THE LOOP
+  
+  # LOOP
+  time <- proc.time()[3]
+  for(i in seq(length(dlist))) {
+    # PRINT
+    cat(i,"")
+    
+    # EXTRACT THE DATA
+    data <- dlist[[i]]
+    
+    # DEFINE THE CROSSBASIS
+    argvar <- list(fun=varfun,knots=quantile(data$tmean_mean,varper/100,na.rm=T),
+                   degree=vardegree)
+    cb <- dlnm::crossbasis(data$tmean_mean,lag=lag,argvar=argvar,
+                           arglag=list(knots=logknots(lag,lagnk)))
+    
+    # RUN THE MODEL AND OBTAIN PREDICTIONS
+    # NB: NO CENTERING NEEDED HERE, AS THIS DOES NOT AFFECT COEF-VCOV
+    model <- glm(formula,data,family=quasipoisson,na.action="na.exclude")
+    options(warn=1)
+    
+    #centered on average temperature
+    cen <- mean(data$tmean_mean,na.rm=T)
+    pred <- crosspred(cb,model,cen=cen, by=1)
+    
+    # REDUCTION TO OVERALL CUMULATIVE (default)
+    red <- crossreduce(cb,model,cen=cen)
+    
+    coef[i,] <- coef(red)
+    vcov[[i]] <- vcov(red)
+    
+  }
+  proc.time()[3]-time
+  
+
+}
+
+
